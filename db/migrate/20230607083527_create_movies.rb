24aa4473aaffa7fec3f8bb9324a8e00157dbe5a0
dantezorenge <orenged524@gmail.com>
2023-06-07 12:40:33 +0300

    changes to movie migration

--- db/migrate/20230607083527_create_movies.rb
+++ db/migrate/20230607083527_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[6.1]
   def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :movie_duration
+      t.string :movie_description
+      t.string :image_url
+      t.string :movie_genre
   end
 end
