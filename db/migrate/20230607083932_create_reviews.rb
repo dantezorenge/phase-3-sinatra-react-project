482e2a5d1644b1e5d00351da56964a3357320239
dantezorenge <orenged524@gmail.com>
2023-06-07 12:48:58 +0300

    new update

--- db/migrate/20230607083932_create_reviews.rb
+++ db/migrate/20230607083932_create_reviews.rb
@@ -1,4 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :movie_reviewer
+      t.string :review
+      t.string :number_of_reviews
   end
 end
