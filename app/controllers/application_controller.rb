f5039246c4da7e35f8ae3739f23bc97ab7558c40
dantezorenge <orenged524@gmail.com>
2023-06-08 09:36:06 +0300

    new changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,90 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  #Add yur routes here
+
+  get '/movies' do 
+    mov = Movie.all
+    mov.to_json
+  end
+
+  get '/reviews' do 
+    rev = Review.all
+    rev.to_json
+  end
+
+  get '/viewers' do 
+    view = Viewer.all
+    view.to_json
+  
+  end
+
+
+  post '/movies' do 
+    movie = Movie.create(
+      title: params[:title],
+      duration: params[:duration],
+      description: params[:description],
+      imageurl: params[:imageurl],
+      genre: params[:genre],
+    )
+  end
+
+  post '/reviews' do 
+    review = Review.create(
+      reviewer: params[:reviewer],
+      review: params[:review],
+      number_of_reviews: params[:number_of_reviews],
+      likes: params[:likes]
+    )
+  
+  end
+
+
+  post '/viewers' do 
+    viewer =Viewer.create(
+      name: params[:name],
+      email: params[:email],
+    )
+  end 
+
+  patch '/viewers/:id' do
+    viewer =Viewer.find(params[:id])
+    viewer.update(
+      name: params[:name],
+      email: params[:email],
+    )
+  end 
+
+
+  patch '/Reviews/:id' do
+    review =Review.find(params[:id])
+    review.update(
+      likes: params[:likes]
+    )
+  end
+  
+
+  delete '/movies/:id' do  
+    movie =Movie.find(params[:id])
+    movie.delete
+    movie.to_json
+  
+  end
+
+  delete '/reviews/:id' do
+    review =Review.find(params[:id])
+    review.delete
+    review.to_json
+  end
+
+
+
+  delete '/viewers/:id' do 
+    viewer =Viewer.find(params[:id])
+    viewer.delete
+    viewer.to_json
   end
 
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,21 @@ require 'net/http'
 require 'json'
 require 'faker'
 
+puts "Seeding Viewers..." 
+50.times do 
+    Viewer.create(
+        name: Faker::Name.name,
+        email: Faker::Internet.email
+    )
+end
+puts"done seeding viewers..." 
 
-puts "🌱 Seeding spices..."
-
-puts "✅ Done seeding!"
+puts "Seeding Reviews..." 
+50.times do 
+    Review.create(
+        reviewer: Faker::Lorem.sentence,
+        review: Faker::Lorem.sentence,
+        number_of_reviews: Faker::Number.between(from: 10, to: 50),
+        likes: Faker::Number.between(from: 1, to: 44)
+    )
+end
